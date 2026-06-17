cf4fb37e4662948becca3953ccf6a5a37b5cd001
YagiTatsunori <129924653+YagiTatsunori@users.noreply.github.com>
2026-06-17 10:42:22 +0900

    Add files via upload

---
+++ Fig2.R
@@ -0,0 +1,37 @@
+library(ggplot2)
+type2_func <- function(B,BB = 0,BL = 0.56,BT = 0.8,delta1 = 0.5,delta2 = 0.4,delta3 = 0.4,AAV = 1){
+  p <- ifelse(B <= BB,Inf,
+              ifelse(B >= BL,delta1,
+                     delta1+delta2*exp(delta3*log(1+AAV^2))*(BL-B)/(B-BB)
+              ))
+  alpha <- exp(p*(B-BT))
+  return(alpha)
+}
+
+I <- seq(0,1,by = 0.01)
+data <- data.frame(I = I,alpha = type2_func(B = I),beta = type2_func(B = I,BT = 0.6),gannma = type2_func(B = I,BT = 0.4))
+ggplot(data, aes(x = I)) +
+  geom_line(aes(y = alpha, linetype = "alpha"), linewidth = 2) +
+  geom_line(aes(y = beta,  linetype = "beta"), linewidth = 2) +
+  geom_line(aes(y = gannma,linetype = "gannma"), linewidth = 2) +
+  scale_linetype_manual(values = c("solid", "dashed", "dotted")) +
+  geom_segment(x = 0.56,xend = 0.56,y = 0,yend = 0.8869204,linetype = "31",color = "black") +
+  geom_segment(x = 0.8,xend = 0.8,y = 0,yend = 1,linetype = "31",color = "black") +
+  geom_segment(x = 0,xend = 0.8,y = 1,yend = 1,linetype = "31",color = "black") +
+  scale_x_continuous(expand = expansion(mult = c(0,0.1)),breaks = c(0,0.5,1),limits = c(0,1)) +
+  scale_y_continuous(expand = expansion(mult = c(0,0.1)),breaks = c(0,0.5,1,1.5),limits = c(0,1.2)) +
+  labs(x = expression("Stock biomass level (" * D[y] * ")"),
+       y = expression("Catch multiplier (" * alpha[y] * ")")) +
+  theme_classic() +
+  annotate("text",x = 0.84,y = 0.07,label = "B[T]",parse = TRUE,size = 8) +
+  annotate("text",x = 0.6,y = 0.07,label = "B[L]",parse = TRUE,size = 8) +
+  annotate("text",x = 0.05,y = 0.5,label = "B[B]",parse = TRUE,size = 8) +
+  theme(legend.position = "none",
+        axis.text = element_text(size = 25,color = "black"),
+        axis.title = element_text(size = 25,color = "black"),
+        axis.line = element_line(colour = "black",linewidth = 1,lineend = "square"),
+        legend.text = element_text(size = 25),legend.title = element_text(size = 25),
+        legend.key.spacing.y = unit(1,'lines'),axis.ticks.length = unit(0.3,"cm"),
+        plot.title = element_text(size = 30)) + ggtitle("")
+ggsave(paste0("./Figs2.jpg"),
+       width = 340, height = 170, units = "mm", dpi = 300)
--- Fig4.R
+++ Fig4.R
@@ -1,73 +1,82 @@
-FIG_4_data <- matrix(0,30,4)
-FIG_4_data[,1] <- parameters$fish
-FIG_4_data[,2] <- c(rep(rule_set[1],6),rep(rule_set[2],6),rep(rule_set[3],6),rep(rule_set[4],6),rep(rule_set[5],6))
-FIG_4_data[,3] <- rep(c("RC_long","RC_short","RSB_long","RSB_short","AAV","Blim"),5)
-colnames(FIG_4_data) <- c("fish","method","index","value")
-for(i in 1:5){
-  mean_RC_long <- mean(sapply(1:3,function(k) MSE_output[[k]][[i]][[13]]))
-  mean_RC_short <- mean(sapply(1:3,function(k) MSE_output[[k]][[i]][[11]]))
-  mean_RSB_long <- mean(sapply(1:3,function(k) MSE_output[[k]][[i]][[12]]))
-  mean_RSB_short <- mean(sapply(1:3,function(k) MSE_output[[k]][[i]][[10]]))
-  mean_AAV <- mean(sapply(1:3,function(k) MSE_output[[k]][[i]][[14]]))
-  mean_Blim <- mean(sapply(1:3,function(k) MSE_output[[k]][[i]][[15]]))
-  FIG_4_data[(1+(6*(i-1))):(6*i),4] <- rbind(mean_RC_long,mean_RC_short,mean_RSB_long,mean_RSB_short,mean_AAV,mean_Blim)
-}
-FIG4_data <- as.data.frame(FIG_4_data)
-FIG4_data$value <- as.numeric(FIG4_data$value)
-FIG4_data$index <- factor(FIG4_data$index,levels = c("Blim","AAV","RSB_short","RSB_long","RC_short","RC_long"))
-FIG4_data$method <- factor(FIG4_data$method,levels = c(rule_set[5],rule_set[4],rule_set[3],rule_set[2],rule_set[1]))
-write.csv(t(FIG4_data),paste0(directory_name,"/Figs_",parameters$fish,"/Fig4_", parameters$fish,".csv"))
+Fig4_func <- function(MSE_output,parameters,scale_upper,scale_down,title_name){
+  FIG_4_data <- matrix(0,30,4)
+  FIG_4_data[,1] <- parameters$fish
+  FIG_4_data[,2] <- c(rep(rule_set[1],6),rep(rule_set[2],6),rep(rule_set[3],6),rep(rule_set[4],6),rep(rule_set[5],6))
+  FIG_4_data[,3] <- rep(c("RC_long","RC_short","RSB_long","RSB_short","AAV","Blim"),5)
+  colnames(FIG_4_data) <- c("fish","method","index","value")
+  for(i in 1:5){
+    mean_RC_long <- mean(sapply(1:3,function(k) MSE_output[[k]][[i]][[13]]))
+    mean_RC_short <- mean(sapply(1:3,function(k) MSE_output[[k]][[i]][[11]]))
+    mean_RSB_long <- mean(sapply(1:3,function(k) MSE_output[[k]][[i]][[12]]))
+    mean_RSB_short <- mean(sapply(1:3,function(k) MSE_output[[k]][[i]][[10]]))
+    mean_AAV <- mean(sapply(1:3,function(k) MSE_output[[k]][[i]][[14]]))
+    mean_Blim <- mean(sapply(1:3,function(k) MSE_output[[k]][[i]][[15]]))
+    FIG_4_data[(1+(6*(i-1))):(6*i),4] <- rbind(mean_RC_long,mean_RC_short,mean_RSB_long,mean_RSB_short,mean_AAV,mean_Blim)
+  }
+  FIG4_data <- as.data.frame(FIG_4_data)
+  FIG4_data$value <- as.numeric(FIG4_data$value)
+  FIG4_data$index <- factor(FIG4_data$index,levels = c("Blim","AAV","RSB_short","RSB_long","RC_short","RC_long"))
+  FIG4_data$method <- factor(FIG4_data$method,levels = c(rule_set[5],rule_set[4],rule_set[3],rule_set[2],rule_set[1]))
+  write.csv(t(FIG4_data),paste0("./Figs_",parameters$fish,"/Fig4_", parameters$fish,".csv"))
+  
+  FIG4_data <- FIG4_data %>% mutate(method = recode(method,
+                                                    "rfb_rule" = "rfb rule",
+                                                    "type2_rule" = "type2 rule",
+                                                    "rfb_Cave" = "rfb + c\u0304",
+                                                    "type2_f" = "type2 + f",
+                                                    "chr_rule" = "chr rule"))
+  
+  rule_set <- recode(rule_set,
+                     "rfb_rule" = "rfb rule",
+                     "type2_rule" = "type2 rule",
+                     "rfb_Cave" = "rfb + c\u0304",
+                     "type2_f" = "type2 + f",
+                     "chr_rule" = "chr rule")
+  
+  p <- ggplot(data = FIG4_data[FIG4_data$index != "AAV" & FIG4_data$index != "Blim",],aes(x = index,y = value,group = method)) +
+    geom_hline(yintercept = 1,linetype = "solid",linewidth = 2,color = "black") +
+    scale_colour_manual(values = c("rfb rule" = "red","rfb + c\u0304" = "orange","type2 rule" = "blue","type2 + f" = "cyan3","chr rule" = "limegreen"),
+                        guide  = guide_legend(reverse = TRUE),name = NULL)
 
-FIG4_data <- FIG4_data %>% mutate(method = recode(method,
-                                                  "rfb_rule" = "rfb rule",
-                                                  "type2_rule" = "type2 rule",
-                                                  "rfb_Cave" = "rfb + c\u0304",
-                                                  "type2_f" = "type2 + f",
-                                                  "chr_rule" = "chr rule"))
-
-rule_set <- recode(rule_set,
-                   "rfb_rule" = "rfb rule",
-                   "type2_rule" = "type2 rule",
-                   "rfb_Cave" = "rfb + c\u0304",
-                   "type2_f" = "type2 + f",
-                   "chr_rule" = "chr rule")
-
-p <- ggplot(data = FIG4_data[FIG4_data$index != "AAV" & FIG4_data$index != "Blim",],aes(x = index,y = value,group = method)) +
-  geom_hline(yintercept = 1,linetype = "solid",linewidth = 2,color = "black") +
-  scale_colour_manual(values = c("rfb rule" = "red","rfb + c\u0304" = "orange","type2 rule" = "blue","type2 + f" = "cyan3","chr rule" = "limegreen"),
-                      guide  = guide_legend(reverse = TRUE),name = NULL) +
-  scale_linetype_manual(values = c("origin" = "solid","optimized" = "dashed"))
-
-
-for (method in rev(rule_set)){
+  for (method in rev(rule_set)){
+    p <- p +
+      geom_line(data = FIG4_data[FIG4_data$index != "AAV" & FIG4_data$index != "Blim" & FIG4_data$method == method,],aes(colour = method),linewidth = 2) +
+      geom_point(data = FIG4_data[FIG4_data$index != "AAV" & FIG4_data$index != "Blim" & FIG4_data$method == method,],aes(colour = method),size = 3)
+  }
+  
   p <- p +
-    geom_line(data = FIG4_data[FIG4_data$index != "AAV" & FIG4_data$index != "Blim" & FIG4_data$method == method,],aes(colour = method),linewidth = 2) +
-    geom_point(data = FIG4_data[FIG4_data$index != "AAV" & FIG4_data$index != "Blim" & FIG4_data$method == method,],aes(colour = method),size = 3)
+    coord_flip() +
+    scale_x_discrete(expand = expansion(mult = c(0,0.1))) +
+    scale_y_continuous(expand = expansion(mult = c(0,0.1)),breaks = c(0,1,scale_upper),limits = c(0,scale_upper)) +
+    theme_classic() + labs(x = "",y = "") +
+    theme(plot.title = element_text(hjust = -0.35,vjust = 0,size = 20,color = "black"),
+          legend.position = "none",axis.text = element_text(size = 20,color = "black"),
+          axis.title = element_text(size = 20,color = "black"),
+          axis.line = element_line(colour = "black",linewidth = 1,lineend = "square"),
+          axis.ticks.length = unit(0.3,"cm"))
+  
+  q <- ggplot(data = FIG4_data[FIG4_data$index == "AAV",],aes(x = index,y = value,group = method)) +
+    geom_point(data = FIG4_data[FIG4_data$index == "AAV",],aes(colour = method),size = 4) +
+    scale_colour_manual(values = c("rfb rule" = "red","rfb + c\u0304" = "orange","type2 rule" = "blue","type2 + f" = "cyan3","chr rule" = "limegreen"),
+                        guide  = guide_legend(reverse = TRUE),name = NULL) +
+    coord_flip() +
+    scale_y_continuous(expand = expansion(mult = c(0,0.1)),breaks = c(0,0.5),limits = c(0,scale_down),oob = scales::oob_squish) +
+    theme_classic() + labs(x = expression(), y = "") +
+    theme(plot.title.position = "plot",
+          plot.title = element_text(hjust = 0,vjust = 0,size = 25,color = "black"),
+          legend.position = "none",axis.text = element_text(size = 20,color = "black"),
+          axis.title = element_text(size = 20,color = "black"),
+          axis.line = element_line(colour = "black",linewidth = 1,lineend = "square"),
+          axis.ticks.length = unit(0.3,"cm"))
+  
+  (p/q) +
+    plot_annotation(
+      title = title_name,
+      theme = theme(
+        plot.title = element_text(hjust = 0, vjust = 1, size = 25),
+        plot.margin = margin(5,5,-20,0)
+      )
+    ) +
+    plot_layout(heights = c(6,1))
+  ggsave(paste0("./Figs_",parameters$fish,"/Fig4_",parameters$fish,".jpg"),width = 170,height = 170,units = "mm",dpi = 300)
 }
-
-p <- p +
-  coord_flip() +
-  scale_x_discrete(expand = expansion(mult = c(0,0.1))) +
-  scale_y_continuous(expand = expansion(mult = c(0,0.1)),breaks = c(0,1,scale_upper),limits = c(0,scale_upper)) +
-  theme_classic() + labs(x = "",y = "") +
-  theme(plot.title = element_text(hjust = -0.35,vjust = 0,size = 20,color = "black"),
-        legend.position = "none",axis.text = element_text(size = 20,color = "black"),
-        axis.title = element_text(size = 20,color = "black"),
-        axis.line = element_line(colour = "black",linewidth = 1,lineend = "square"),
-        axis.ticks.length = unit(0.3,"cm"))
-
-q <- ggplot(data = FIG4_data[FIG4_data$index == "AAV",],aes(x = index,y = value,group = method)) +
-  geom_point(data = FIG4_data[FIG4_data$index == "AAV",],aes(colour = method),size = 4) +
-  scale_colour_manual(values = c("rfb rule" = "red","rfb + c\u0304" = "orange","type2 rule" = "blue","type2 + f" = "cyan3","chr rule" = "limegreen"),
-                      guide  = guide_legend(reverse = TRUE),name = NULL) +
-  coord_flip() +
-  scale_y_continuous(expand = expansion(mult = c(0,0.1)),breaks = c(0,0.5),limits = c(0,scale_down),oob = scales::oob_squish) +
-  theme_classic() + labs(x = expression(), y = "") +
-  theme(plot.title.position = "plot",
-        plot.title = element_text(hjust = 0,vjust = 0,size = 25,color = "black"),
-        legend.position = "none",axis.text = element_text(size = 20,color = "black"),
-        axis.title = element_text(size = 20,color = "black"),
-        axis.line = element_line(colour = "black",linewidth = 1,lineend = "square"),
-        axis.ticks.length = unit(0.3,"cm"))
-(p/q) + plot_annotation(title = title_name,theme = theme(plot.title = element_text(hjust = 0,vjust = 1,size = 25),plot.margin = margin(5,5,-20,0))) + plot_layout(heights = c(6,1))
-ggsave(paste0(directory_name,"/Figs_",parameters$fish,"/Fig4_",parameters$fish,".jpg"),width = 170,height = 170,units = "mm",dpi = 300)
